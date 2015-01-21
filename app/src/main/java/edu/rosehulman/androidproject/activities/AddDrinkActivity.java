d9f0704f6e547dbd45cfcfab5d48b40617d83d5d
System Administrator <root@johans-mbp.wlan.rose-hulman.edu>
2015-01-21 00:07:06 -0500

    add ui

--- app/src/main/java/edu/rosehulman/androidproject/activities/AddDrinkActivity.java
+++ app/src/main/java/edu/rosehulman/androidproject/activities/AddDrinkActivity.java
@@ -1,9 +1,18 @@
 package edu.rosehulman.androidproject.activities;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import edu.rosehulman.androidproject.R;
 
 /**
  * Created by palssoa on 1/12/2015.
  */
 public class AddDrinkActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_drink);
+    }
 }
--- app/src/main/java/edu/rosehulman/androidproject/fragments/HomeFragment.java
+++ app/src/main/java/edu/rosehulman/androidproject/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package edu.rosehulman.androidproject.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.rosehulman.androidproject.R;
+import edu.rosehulman.androidproject.activities.AddDrinkActivity;
 
 /**
  * Created by palssoa on 12/20/2014.
@@ -19,6 +21,12 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_home, container, false);
+        rootView.findViewById(R.id.fragment_home_button_drink).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), AddDrinkActivity.class));
+            }
+        });
 
 
         return rootView;
