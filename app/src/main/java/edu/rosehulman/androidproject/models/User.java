03c4abbdbd31d54ed529e8329d32739a3910807c
System Administrator <johan.kitti@gmail.com>
2015-01-25 23:01:53 -0500

    more

--- app/src/main/java/edu/rosehulman/androidproject/models/User.java
+++ app/src/main/java/edu/rosehulman/androidproject/models/User.java
@@ -8,9 +8,26 @@ import java.util.List;
  */
 public class User {
 
+    private int mCaffeineLevel;
     private List<Drink> mDrinkHistory;
 
     public User() {
         mDrinkHistory = new ArrayList<>();
+    }
+
+    public int getmCaffeineLevel() {
+        return mCaffeineLevel;
+    }
+
+    public void setmCaffeineLevel(int mCaffeineLevel) {
+        this.mCaffeineLevel = mCaffeineLevel;
+    }
+
+    public List<Drink> getmDrinkHistory() {
+        return mDrinkHistory;
+    }
+
+    public void setmDrinkHistory(List<Drink> mDrinkHistory) {
+        this.mDrinkHistory = mDrinkHistory;
     }
 }
