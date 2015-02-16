3ebd4c210590fbb30d31a080c4ad3c9ebb81e0b0
Andreas Pålsson <apals@kth.se>
2015-02-15 19:56:32 -0500

    change user list fragment to use expandablelistadapter

--- app/src/main/java/edu/rosehulman/androidproject/adapters/UserListAdapter.java
+++ app/src/main/java/edu/rosehulman/androidproject/adapters/UserListAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,64 +21,100 @@ import edu.rosehulman.androidproject.ExpandAnimation;
 import edu.rosehulman.androidproject.GraphUtils;
 import edu.rosehulman.androidproject.R;
 import edu.rosehulman.androidproject.activities.MainActivity;
+import edu.rosehulman.androidproject.models.DateCaffeinePoint;
 import edu.rosehulman.androidproject.models.User;
 
 /**
  * A simple implementation of list adapter.
  */
-public class UserListAdapter extends ArrayAdapter<User> {
+public class UserListAdapter extends BaseExpandableListAdapter {
 
     private Activity context;
     private GraphicalView mLineChart;
+    private List<User> users;
 
 
 
-    public UserListAdapter(Context context, int textViewResourceId, List<User> objects) {
-        super(context, textViewResourceId, objects);
+    public UserListAdapter(Context context, List<User> objects) {
         this.context = (Activity) context;
+        this.users = objects;
+    }
+
+
+
+    @Override
+    public int getGroupCount() {
+        return users.size();
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public int getChildrenCount(int groupPosition) {
+        return 1;
+    }
 
+    @Override
+    public Object getGroup(int groupPosition) {
+        return users.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = context.getLayoutInflater().inflate(R.layout.userlist_row_layout, null);
             final View a = convertView;
-
-            //TODO: the following code should probably be in onitemclick up there but the collapse of the item didn't work
-            convertView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    LinearLayout toolbar = (LinearLayout) a.findViewById(R.id.toolbar);
-
-                    XYMultipleSeriesDataset dataset = GraphUtils.getDataset((((MainActivity) context).getUsers()));
-                    TimeSeries series = new TimeSeries("lol");
-                    series.add(1,1);
-                    dataset.addSeries(series);
-
-                    XYMultipleSeriesRenderer renderer = GraphUtils.getMultipleSeriesRenderer(context);
-                    renderer.addSeriesRenderer(GraphUtils.getSeriesRenderer(context));
-                    mLineChart = ChartFactory.getTimeChartView(context, dataset, renderer, "HH-mm-ss");
-                    toolbar.addView(mLineChart);
-                    // Creating the expand animation for the item
-                    ExpandAnimation expandAni = new ExpandAnimation(toolbar, 500);
-
-                    // Start the animation on the toolbar
-                    toolbar.startAnimation(expandAni);
-                }
-            });
         }
 
-        ((TextView) convertView.findViewById(R.id.username)).setText(getItem(position).getUsername());
-        ((TextView) convertView.findViewById(R.id.caffeinelevel)).setText(String.format("%.2f", getItem(position).getCaffeineLevel()) + "‰");
-        //((TextView) convertView.findViewById(R.id.subtext_date)).setText(getItem(position).getDrinkHistory().get(getItem(position).getDrinkHistory().size() - 1).getFormattedDate());
+        ((TextView) convertView.findViewById(R.id.username)).setText(((User) getGroup(groupPosition)).getUsername());
+        System.out.println("USELRISTADAPTER: " + ((User) getGroup(groupPosition)).getCaffeineLevel());
+        ((TextView) convertView.findViewById(R.id.caffeinelevel)).setText(String.format("%.2f", ((User) getGroup(groupPosition)).getCaffeineLevel()) + "‰");
+        return convertView;
+    }
 
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = context.getLayoutInflater().inflate(R.layout.userlist_row_layout_child, null);
+            final View a = convertView;
+        }
 
-        // Resets the toolbar to be closed
-        View toolbar = convertView.findViewById(R.id.toolbar);
-        ((LinearLayout.LayoutParams) toolbar.getLayoutParams()).bottomMargin = -100;
-        toolbar.setVisibility(View.GONE);
+        XYMultipleSeriesDataset dataset = GraphUtils.getDataset((((MainActivity) context).getUsers()));
+        TimeSeries series = new TimeSeries("lol");
+        for(DateCaffeinePoint d : ((User) getGroup(groupPosition)).getPoints()) {
+            series.add(d.getDate(), d.getCaffeine());
+        }
+        dataset.addSeries(series);
+
+        XYMultipleSeriesRenderer renderer = GraphUtils.getMultipleSeriesRenderer(context);
+        renderer.addSeriesRenderer(GraphUtils.getSeriesRenderer(context));
+        mLineChart = ChartFactory.getTimeChartView(context, dataset, renderer, "HH:mm");
+        LinearLayout linearLayout = (LinearLayout) convertView.findViewById(R.id.list_container);
+        linearLayout.addView(mLineChart);
 
         return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
     }
 }
